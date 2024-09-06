8ca22d48379089a28ab4ccf4c2278433b2b564ba
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-09-06 23:51:46 +0100

    test auth with postman

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,42 @@
 const express = require('express');
 const authRoutes = require('./routes/authRoutes');
+const mongoose = require('mongoose');
+const path = require('path');
+
 
 const app = express();
 
-app.use(express.json());
+// URL de connexion MongoDB
+const dbURI = 'mongodb+srv://ibtissamerrachidi810:An1XQlEIFReUIl5E@cluster0.ses7k.mongodb.net/Mydatabase';
+
+// Connexion à MongoDB
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('MongoDB connected successfully');
+  // Démarrer le serveur après la connexion réussie
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+})
+.catch((err) => {
+  console.error('Error connecting to MongoDB:', err);
+});
 
 // Middleware pour analyser les données POST
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Définir le répertoire des vues
+app.set('views', path.join(__dirname, 'views/pages'));
 // Définir un moteur de rendu si vous utilisez res.render
 app.set('view engine', 'ejs');
 
 // Routes
 app.use(authRoutes);
 
-// Lancer le serveur
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
-
 module.exports = app;
+
